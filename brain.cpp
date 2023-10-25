554210f1be7e3a553b2fb6c9f6f1950add229fd6
liyxbaby <liyxbaby@proton.me>
2023-10-25 13:46:11 +0000

    new graph

--- brain.cpp
+++ brain.cpp
@@ -18,4 +18,14 @@ DQN::DQN()
         lastReward = Utils::create2DArray(1,1);
         action = 0;
         batchState = Utils::create2DArray(nSamples, nStates);
-        batchNextState = Utils::create2DArray(nSa
+        batchNextState = Utils::create2DArray(nSamples, nStates);
+        batchReward = Utils::create2DArray(nSamples, 1);
+        batchAction = Utils::create2DArrayOfIntegers(nSamples, 1);
+        randomIndices = Utils::create2DArrayOfIntegers(nSamples, 1);
+}
+
+int DQN::update(float** reward, float** newState)
+{
+	//std::cout << "holy fucking shit" << std::endl;
+
+    if(memory.cou
