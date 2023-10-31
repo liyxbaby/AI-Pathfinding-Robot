453c75a1c8bdad9e46dcd05fa8e915b557ce7162
liyxbaby <liyxbaby@proton.me>
2023-10-31 12:05:41 +0000

    workqueue: use @pool instead of @gcwq or @cpu where applicable

--- config.hpp
+++ config.hpp
@@ -5,4 +5,14 @@ class Config
 {
     public:
     const static int nStates = 3;
-    const static int nActions 
+    const static int nActions = 3;
+    const static int nNeurons = 10;
+    constexpr static float gamma = 0.9;
+    constexpr static float learningRate = 0.001;
+    const static int nSamples = 25;
+    const static int memoryCapacity = 10000;
+    const static int softmaxTemperature = 10;
+    const static int learningIterations = 2500;
+};
+
+#end
