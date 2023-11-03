6f108dc268d7bc83952ffec76e30f389d756bc62
liyxbaby <liyxbaby@proton.me>
2023-11-03 06:56:59 +0000

    More tidying up

--- memory.cpp
+++ memory.cpp
@@ -17,4 +17,16 @@ Memory::Memory()
         states = Utils::create2DArray(size, nStates);
     	nextStates = Utils::create2DArray(size, nStates);
     	rewards = Utils::create2DArray(size, 1);
-    	actions = Utils::create2DArra
+    	actions = Utils::create2DArrayOfIntegers(size, 1);
+}
+
+void Memory::push(float** state, float** nextState, float** reward, int** action)
+{
+    if (counter == size)
+    {
+        full = true;
+        counter = 0;
+    }
+    for (int i = 0 ; i < nStates ; i++)
+    {
+        states[counter][i] = state[0][
