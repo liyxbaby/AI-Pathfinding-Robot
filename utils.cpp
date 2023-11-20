386c09026ab842955d3572bd510be428ff48dbeb
liyxbaby <liyxbaby@proton.me>
2023-11-20 11:49:58 +0000

    Update js

--- utils.cpp
+++ utils.cpp
@@ -3,3 +3,16 @@
 #include "config.hpp"
 #include "utils.hpp"
 #include <math.h>
+using namespace std;
+
+// FUNCTION FOR DECODING STATES INCOMING FROM UART COMMANDS
+void Utils::stateDecoder(float ** state, string command)
+{
+for (int i=0; i<=Config::nStates; i++)
+    if(command[i] == '1')
+        state[0][i] = 1;
+    else
+        state[0][i] = 0;
+}
+
+// FUNC
