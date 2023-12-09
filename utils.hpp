4f88f2bebad0a62612f70984a6203f411d89ebae
liyxbaby <liyxbaby@proton.me>
2023-12-09 06:56:03 +0000

    usb hub: send clear_tt_buffer_complete events when canceling TT clear work

--- utils.hpp
+++ utils.hpp
@@ -11,4 +11,8 @@ class Utils
         static void    clear(float** x, int r, int c);
         static void    clearIntegers(int** x, int r, int c);
         static int**   create2DArrayOfIntegers(int r, int c);
-        static void    dot(fl
+        static void    dot(float** result, float ** x, float ** y, int m, int n, int p);
+        static void    sum(float ** x, float ** y, int r, int c);
+        static void    copy(float ** x, float ** y, int r, int c);
+        static void    distance(float** result, float ** x,  int r, int c);
+        static void    relu(float ** x, int 
